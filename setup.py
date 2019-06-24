e4ce2bef989892eca2ccd6b829c584b7a743a2d6
sinbat85 <35217419+sinbat85@users.noreply.github.com>
2019-06-24 08:44:58 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+setuptools.setup(
+    name="pycalc",
+    version="1.0",
+    author="Andrei Baturov",
+    description="Python command-line calculator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.epam.com/Andrei_Baturov/python_course/tree/master/final",
+    packages=("pycalc",),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Maths"
+    ],
+    entry_points={
+        'console_scripts':
+            ['pycalc = pycalc.__main__:_main']
+        },
+)
